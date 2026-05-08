7748beaddc1013785b29cfe190675e1e6812201b
Jiffdahli <j.c.martinez@t-online.de>
2026-05-08 16:26:56 +0200

    New structure WIP

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,1 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import CatalogPage from "./pages/CatalogPage";
+import ProfilePage from " ./pages/ProfilePage";
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/catalog" element={<CatalogPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
