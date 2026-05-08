f62de7e3e6a4cc3289111a2b5f868411febc589f
Jiffdahli <j.c.martinez@t-online.de>
2026-05-08 15:19:58 +0200

    neue Struktur

--- frontend/Components/Header/AuthContainer/LoginLogouButton/LoginLogoutButton.tsx
+++
@@ -1,8 +0,0 @@
-function LoginLogoutButton() {
-    return (
-        <button className="login-logout-button">
-            Login/Logout
-        </button>
-    );
-}
-export default LoginLogoutButton;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,1 @@
-import Header from '../Components/Header/header'
-import './App.css'
 
-function App() {
-  return (
-    <Header />
-  )
-}
-
-export default App
